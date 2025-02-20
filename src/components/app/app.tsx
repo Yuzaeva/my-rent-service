753540aa6fe07a8edf2904a8b59c17bc19326e85
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2025-02-20 13:08:25 +0300

    Update app.tsx маршрутизация в приложении

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
 import MainPage from "../../pages/main-page/main-page";
+import Favorites from "../../pages/favorites/favorites";
+import Login from "../../pages/login/login";
+import Offer from "../../pages/offer/offer";
+import { PrivateRoute } from "../private-route/private-route";
+import PageNotFound from "../../pages/page-not-found/page-not-found";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { AppRoute } from "../../const";
+import { AuthorizationStatus } from "../../const";
 import {JSX} from 'react';
 
 type AppMainPageProps = {
@@ -7,7 +15,33 @@ type AppMainPageProps = {
 
 function App({rentalOffersCount}: AppMainPageProps): JSX.Element{
     return (
-        <MainPage rentalOffersCount={rentalOffersCount}/>
+        <BrowserRouter>
+        <Routes>
+            <Route
+            path={AppRoute.Main}
+            element={<MainPage rentalOffersCount={rentalOffersCount}/>}/>
+            <Route
+            path={ AppRoute.Favorites }
+            element={
+              <PrivateRoute
+                authorizationStatus={ AuthorizationStatus.NoAuth }
+              >
+                <Favorites />
+
+              </PrivateRoute>
+            }
+          />
+            <Route
+            path={AppRoute.Offer}
+            element={<Offer/>}/>
+            <Route
+            path={AppRoute.Login}
+            element={<Login/>}/>
+            <Route
+            path="*"
+            element={<PageNotFound/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 export default App;
