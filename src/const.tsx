c5d717b3e901f4ab138e7fbfa42d8dbe9f43136c
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2025-02-20 13:09:08 +0300

    Update const.tsx маршруты

--- src/const.tsx
+++ src/const.tsx
@@ -1,4 +1,22 @@
+import PageNotFound from "./pages/page-not-found/page-not-found";
+import Favorites from "./pages/favorites/favorites";
+import Login from "./pages/login/login";
+import Offer from "./pages/offer/offer";
+
 const Setting = {
     rentOffersCount: 312,
 } as const;
-export {Setting};
+
+const AppRoute = {
+    Main: '/',
+    Login: '/login',
+    Favorites: '/favorites',
+    Offer: '/offer/:id',
+} as const;
+
+const AuthorizationStatus = {
+    Auth: 'AUTH',
+    NoAuth: 'NO_AUTH',
+    Unknown: 'UNKNOWN',
+}
+export {Setting, AppRoute, AuthorizationStatus};
