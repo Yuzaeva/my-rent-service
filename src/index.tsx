3929010fe57b9276bd1f2fa810503fdc97b55fe2
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2025-03-06 01:07:52 +0300

    Update index.tsx импортируем моки

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {Setting} from './const';
+import { offers } from './mock/offers';
+import { offersList } from './mock/offers-list';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App rentalOffersCount={Setting.rentOffersCount}/>
+    <App rentalOffersCount={Setting.rentOffersCount}
+    offersList={offersList}
+    offers = {offers}/>
   </React.StrictMode>
 );
