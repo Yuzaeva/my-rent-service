bcb1e10a7247f752cf7db35f80942920a43be215
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2025-03-26 22:45:37 +0300

    Update cities-card-list.tsx

--- src/components/cities-card-list/cities-card-list.tsx
+++ src/components/cities-card-list/cities-card-list.tsx
@@ -3,14 +3,20 @@ import CitiesCard from '../cities-card/cities-card';
 
 type CitiesCardListProps = {
     offersList: OffersList[];
+    onCardHover?: (id: string | null) => void;
+    onListItemHover?: (offerId: string) => void;
 };
 
-function CitiesCardList({offersList}: CitiesCardListProps){
+function CitiesCardList({offersList, onCardHover}: CitiesCardListProps){
     return(
         <div className="cities__places-list places__list tabs__content">
             {Array.from(offersList, (item) => 
             <CitiesCard key={item.id} id={item.id} title={item.title} type={item.type} price={item.price}
-        previewImage={item.previewImage} isPremium={item.isPremium} rating={item.rating}/>)}
+                previewImage={item.previewImage} isPremium={item.isPremium} rating={item.rating}
+                onMouseEnter={() => onCardHover?.(item.id)}
+                onMouseLeave={() => onCardHover?.(null)} onHover={function (title: string): void {
+                    throw new Error('Function not implemented.');
+                } }/>)}
         </div>
         );
 }
