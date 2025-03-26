909b31905252ff90fccc0819e8e6e7a76de66cf5
Yuzaeva <163466856+Yuzaeva@users.noreply.github.com>
2025-03-26 22:56:28 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import App from './components/app/app';
 import {Setting} from './const';
 import { offers } from './mock/offers';
 import { offersList } from './mock/offers-list';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,8 +13,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App rentalOffersCount={Setting.rentOffersCount}
     offersList={offersList}
     offers = {offers}/>
+    </Provider>
   </React.StrictMode>
 );
